d6ebd4414941246e3c3c2337d36ea17fab5a39b5
Rodrigo Guerrero <rodrigo.guerrero@warnermedia.com>
2023-07-31 17:48:15 +0200

    Revert kotlin compiler extension version.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
     packagingOptions {
         resources {
