79e2cf1280f44b38d6f1afce2c9cdbdb4da3dde2
Rodrigo Guerrero <rodrigo.guerrero@warnermedia.com>
2023-10-10 13:57:36 +0200

    fix meta inf

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     }
     packagingOptions {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "**/META-INF/**"
         }
     }
     buildTypes {
