25a454cbd3d43132844cd132de6fcae17abaa7fe
Rodrigo Guerrero <rodrigo.guerrero@warnermedia.com>
2025-05-12 12:40:31 +0200

    chore: bump version.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rodrigoguerrero.myweather"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
     }
     buildFeatures {
         compose = true
